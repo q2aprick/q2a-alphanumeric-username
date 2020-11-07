d0359e3cbebc35ab7cf9d49547f5f37103ae05ae
q2aprick <74082290+q2aprick@users.noreply.github.com>
2020-11-07 19:06:51 +0700

    Update mention.php

--- mention.php
+++ mention.php
@@ -10,7 +10,7 @@ class qa_html_theme_layer extends qa_html_theme_base
 		$this->output('<div class="qa-q-view-content qa-post-content">');
 		
 	//	$this->output_raw($content);
-		$mentionedcontent = preg_replace("/@([A-Za-z0-9_]{1,15})/", " <a href=\"/user/$1\">@$1</a>", $content);
+		$mentionedcontent = preg_replace("/@([A-Za-z0-9_])/", " <a href=\"/user/$1\">@$1</a>", $content);
 		if (strlen($content))
 			echo $mentionedcontent;
 		
@@ -26,7 +26,7 @@ class qa_html_theme_layer extends qa_html_theme_base
 
 		$this->output('<div class="qa-a-item-content qa-post-content">');
 //		$this->output_raw($a_item['content']);
-        $acontent = preg_replace("/@([A-Za-z0-9_]{1,15})/", " <a href=\"/user/$1\">@$1</a>", $a_item['content']);
+        $acontent = preg_replace("/@([A-Za-z0-9_])/", " <a href=\"/user/$1\">@$1</a>", $a_item['content']);
 		if (strlen($acontent))
 			echo $acontent;
 		$this->output('</div>');
@@ -39,7 +39,7 @@ class qa_html_theme_layer extends qa_html_theme_base
 
 		$this->output('<div class="qa-c-item-content qa-post-content">');
 	//	$this->output_raw($c_item['content']);
-	    $ccontent = preg_replace("/@([A-Za-z0-9_]{1,15})/", " <a href=\"/user/$1\">@$1</a>", $c_item['content']);
+	    $ccontent = preg_replace("/@([A-Za-z0-9_])/", " <a href=\"/user/$1\">@$1</a>", $c_item['content']);
 		if (strlen($ccontent))
 			echo $ccontent;
 		$this->output('</div>');
